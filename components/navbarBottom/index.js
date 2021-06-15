56ca2c7bcd2c71467f4f5ba1497ff4edc14a1f2c
Abdullah Al Mamun <mhmamun166009@gmail.com>
2021-06-16 01:49:25 +0600

    Bottom navbar & logo update

--- components/navbarBottom/index.js
+++ components/navbarBottom/index.js
@@ -11,19 +11,33 @@ const index = () => {
             <div className="navbar-fixed-footer d-lg-none">
                 <div className="d-flex justify-content-between button-container">
                     <div>
-                        <button type="button" className="btn shadow-none badge-btn" onClick={() => router.push('/favorite-list')}>
-                            <Icon icon={heart} size={25} />
+                        <button
+                            type="button"
+                            className="btn shadow-none badge-btn"
+                            onClick={() => router.push('/favorite-list')}
+                        >
+                            <Icon icon={heart} size={23} />
                             <span className="badge">0</span>
                         </button>
                     </div>
-                    <div>
+
+                    <div
+                        className="logo-container flex-center flex-column"
+                        onClick={() => router.push('/')}
+                    >
+                        <img src="/assets/logo-sm.png" className="img-fluid" alt="Famefair logo" />
+                    </div>
+
+                    <div className="ml-auto">
                         <button type="button" className="btn shadow-none badge-btn">
-                            <Icon icon={shoppingCart} size={25} />
+                            <Icon icon={shoppingCart} size={23} />
                             <span className="badge">0</span>
                         </button>
-                    </div>
-                    <div>
-                        <button type="button" className="btn shadow-none badge-btn">
+                        <button
+                            type="button"
+                            className="btn shadow-none badge-btn"
+                            onClick={() => router.push('/login')}
+                        >
                             <Icon icon={user} size={25} />
                         </button>
                     </div>

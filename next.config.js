e1a005e4938a7076bf27d845db7b0c612b1d6342
Abdullah Al Mamun <mhmamun166009@gmail.com>
2021-08-02 00:07:25 +0600

    Ghost loader added

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withCss = require('@zeit/next-css')
-const withPurgeCss = require('next-purgecss')
-
-module.exports = withCss(
-    withPurgeCss({
-        purgeCssEnabled: ({ dev, isServer }) => (!dev && !isServer) // Only enable PurgeCSS for client-side production builds
-    })
-)
